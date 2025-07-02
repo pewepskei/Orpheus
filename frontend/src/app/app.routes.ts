80dff9f453ab46730ed980e601897c8f8695a281
Pewep <philip.d.dumoran@gmail.com>
2025-07-02 23:43:44 +0800

    Add route for remote

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Dashboard } from './dashboard/dashboard';
 import { JoinRoom } from './join-room/join-room';
+import { Remote } from './remote/remote';
 
 export const routes: Routes = [
   { path: '', component: Home, data: { animation: 'Home' } },
   { path: 'join', component: JoinRoom, data: { animation: 'Join' } },
   { path: 'room/:code', component: Dashboard, data: { animation: 'Dashboard' } },
+  { path: 'room/:code/remote', component: Remote, data: { animation: 'Remote' } },
 ];
