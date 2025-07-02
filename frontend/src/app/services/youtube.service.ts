0bff8a5783b37450ab564b1d92de9cfd90715d6a
Pewep <philip.d.dumoran@gmail.com>
2025-07-02 23:44:43 +0800

    Add youtube services

---
+++ frontend/src/app/services/youtube.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface YouTubeVideo {
+  title: string;
+  videoId: string;
+  thumbnail: Array<{ url: string; height: number; width: number }>;
+  url: string;
+  duration: number;
+  formatted_duration: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class YouTubeService {
+  private apiUrl = 'http://192.168.1.109:8000/api/youtube/search/';
+
+  constructor(private http: HttpClient) {}
+
+  search(query: string): Observable<YouTubeVideo[]> {
+    return this.http.post<YouTubeVideo[]>(this.apiUrl, { q: query.trim() });
+  }
+}
+
