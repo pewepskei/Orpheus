7d0949b32f4cb3f2898018f938b2b16f8b050539
Ubuntu <ubuntu@ip-172-31-11-235.ap-southeast-1.compute.internal>
2025-07-17 01:21:37 +0000

    Connect to the Secure Web Socket

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl: 'https://api.pewepskei.dev/api',
   cdnUrl: 'https://cdn.pewepskei.dev',
-  wbsUrl: 'ws://redis.pewepskei.dev/ws/queue',
+  wbsUrl: 'wss://api.pewepskei.dev/ws/queue',
 };
 
